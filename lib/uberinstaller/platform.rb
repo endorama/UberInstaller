0eb9aca454ece718f9696ae477b9767c884127bf
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-07-05 21:37:30 +0200

    add yardoc to platform

--- lib/uberinstaller/platform.rb
+++ lib/uberinstaller/platform.rb
@@ -5,16 +5,22 @@ require 'uberinstaller/logger'
 require 'hash_keyword_args'
 
 module Uberinstaller
+  # http://stackoverflow.com/questions/170956/how-can-i-find-which-operating-system-my-ruby-program-is-running-on
   class Platform
     include Loggable
 
+    # @!attribute [r] architecture
+    #   OS architecture information
+    # @!attribute [r] lsb
+    #   LSB module information
+    # @!attribute [r] uname
+    #   `uname` calls results
     attr_reader :architecture, :lsb, :uname
     
-    # => get platform, detect ubuntu, detect ubuntu version, save lsb params
+    # Get platform, detect ubuntu, detect ubuntu version, save lsb params
     # 
-    # => params:
-    # =>   opts Hash
-    # =>     :lsb => the file containing LSB information
+    # @param opts [Hash]
+    #  :lsb => the file containing LSB information
     def initialize(opts = {})
       @opts = opts.keyword_args(:lsb => '/etc/lsb-release')
       
@@ -27,20 +33,24 @@ module Uberinstaller
       @architecture = @uname[:machine]
     end
 
+    # Check if platform is Ubuntu
     def is_ubuntu?
       return @lsb[:id] == 'Ubuntu' if @lsb[:id]
       logger.fatal 'lsb is not set, impossible to get OS information'
       false
     end
 
+    # Reverse of is_ubuntu?
     def is_not_ubuntu?
       !is_ubuntu?
     end
 
+    # Reverse of is_64bit?
     def is_32bit?
       !is_64bit?
     end
 
+    # Check if system is running 64 bit OS
     def is_64bit?
       return @uname[:machine] == 'x86_64' if @uname[:machine]
       logger.fatal 'uname is not set, impossible to get machine information'
@@ -48,6 +58,10 @@ module Uberinstaller
     end
 
     private
+      # Detect OS architecture information
+      #
+      # Using a call to `uname` try to detect architecture informations.
+      # `uname` must be available on the system
       def get_arch_informations
         @uname ||= Hash.new
         IO.popen 'uname -m' do |io| @uname[:machine] = io.read.strip end
@@ -55,6 +69,9 @@ module Uberinstaller
         IO.popen 'uname -srv' do |io| @uname[:kernel] = io.read.strip end
       end
 
+      # Get OS information from LSB
+      #
+      # LSB must be aavailable on the system
       def get_lsb_informations
         # http://stackoverflow.com/a/1236075/715002
         IO.popen "cat #{@opts.lsb}" do |io|
@@ -78,6 +95,7 @@ module Uberinstaller
         end
       end
 
+      # Handy method to retrieve values from LSB pairs
       def get_lsb_value(string)
         string.split('=')[1].strip
       end
