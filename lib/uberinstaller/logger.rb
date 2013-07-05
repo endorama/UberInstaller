ad27cec514a922cb8df5ec0e30a34f229f618b40
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-07-05 20:16:18 +0200

    add yardocs to logger.rb

--- lib/uberinstaller/logger.rb
+++ lib/uberinstaller/logger.rb
@@ -4,25 +4,48 @@ require 'colored'
 require 'logger'
 
 module Uberinstaller
+
+  # Handle application log
   module Loggable
+    # @!attribute [r] loggers
+    #   Hash of available loggers ( one for class in which logger is invoked )
     @loggers = {}
+    # @!attribute [rw] log_path
+    #   Path in which log files are saved ( default STDOUT )
     @log_path = STDOUT
+    # @!attribute [rw] level
+    #   Log level. Can be one of Logger::DEBUG, Logger::INFO, Logger::WARN, Logger::ERROR ( default Logger::ERROR )
     @level = Logger::ERROR
 
-    # This is the magical bit that gets mixed into your classes
     # Global, memoized, lazy initialized instance of a logger
+    #
+    # This is the magical bit that gets mixed into your classes. Respond to Logger function.
+    # 
+    # @return [Object] an instance of the logger class
     def logger
       classname = (self.is_a? Module) ? self : self.class.name
       @logger ||= Loggable.logger_for(classname)
     end
 
     class << self
+      # @!attribute [rw] level
+      #   Log output level. Can be one of Logger::DEBUG, Logger::INFO, Logger::WARN, Logger::ERROR ( default Logger::ERROR )
+      # @!attribute [rw] log_path
+      #   Path in which log files are saved ( default STDOUT )
       attr_accessor :level, :log_path
       
+      # Return the logger for a specific Class. If the instance is not found, creates it.
+      #
+      # @param classname [String] the name of the class for which a logger instance must be retrieved
+      # @return [Object] the instance of the logger class for the specified Class
       def logger_for(classname)
         @loggers[classname] ||= configure_logger_for(classname)
       end
 
+      # Create and configure a logger for the specified Class
+      #
+      # @param classname [String] the name of the class for which a logger instance must be retrieved
+      # @return [Object] the instance of the logger class for the specified Class
       def configure_logger_for(classname)
         logger = Logger.new(@log_path)
         logger.progname = classname
