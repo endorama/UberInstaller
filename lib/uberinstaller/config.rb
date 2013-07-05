696c84bbab56fd7ce2c594cffcc34c463ff73ff8
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-07-05 23:12:24 +0200

    add package manager infrastructure

--- lib/uberinstaller/config.rb
+++ lib/uberinstaller/config.rb
@@ -7,14 +7,21 @@ module Uberinstaller
   # Shared configuration for Uberinstaller
   module Config
     class << self
-      attr_accessor :local_pkg_path
+      attr_accessor :local_package_manager, :local_pkg_path, :remote_package_manager
 
-      attr_reader :app_name, :app_version, :utils_file_path
+      attr_reader :app_name, :app_version
     end
 
     # @!attribute [rw] local_pkg_path
     #   absolute path in which local package for the configuration file are found
     @local_pkg_path = nil
+    # @!attribute [rw] remote_package_manager
+    #   the package manager used to install system type packages
+    @remote_package_manager = 'apt'
+    # @!attribute [rw] local_package_manager
+    #   the package manager used to install local type packages
+    @local_package_manager = 'dpkg'
+
 
     # @!attribute [r] app_name
     #   Application name
---
+++ lib/uberinstaller/package_manager.rb
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+
+require 'uberinstaller/logger'
+
+require 'open3'
+
+module Uberinstaller
+  module PackageManager
+    class Base
+      include Loggable
+
+      attr_reader :commands
+
+      def initialize
+        @commands = Hash.new
+        @commands = {
+          :add_repository => nil,
+          :info => nil,
+          :install => nil,
+          :search => nil,
+          :update => nil,
+          :upgrade => nil
+        }
+      end
+
+      def debug(action, args = [])
+        logger.debug 'PackageManager action: ' + action.to_s
+        logger.debug 'PackageManager args: ' + args.join(', ') unless args.empty?
+      end
+
+      def method_missing(m, *args, &block)
+        if @commands.has_key? m
+          debug m, args
+
+          logger.info "Running action: #{m}"
+
+          Open3.popen3(@commands[m]) { |stdin, stdout, stderr, wait_thr|
+            pid = wait_thr.pid # pid of the started process.
+            logger.debug "Running pid: #{pid}"
+
+            logger.debug stdout.readlines
+
+            exit_status = wait_thr.value.to_i # Process::Status object returned.
+            logger.debug "Exit status: #{exit_status}"
+            logger.error 'Some error happended during execution:' unless exit_status == 0
+            logger.error stderr.readlines unless exit_status == 0
+          }
+        else
+          puts "There's no method called #{m} here -- please try again."  
+        end
+      end  
+    end
+  end
+end
+
+
+require 'uberinstaller/package_managers/apt'
